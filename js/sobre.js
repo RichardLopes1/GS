227369e49796eaea44705459c2ae2b2993a57526
ThiagoThmaz <thiago.conceicao@sptech.school>
2024-06-05 17:32:59 -0300

    Atualizando_css

--- js/sobre.js
+++ js/sobre.js
@@ -1,18 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('button');
-
-  buttons.forEach(button => {
-      button.addEventListener('click', () => {
-          const hiddenText = button.nextElementSibling;
-          const arrow = button.querySelector('.arrow');
-
-          if (hiddenText.style.display === 'none' || !hiddenText.style.display) {
-              hiddenText.style.display = 'block';
-              arrow.style.transform = 'rotate(90deg)';
-          } else {
-              hiddenText.style.display = 'none';
-              arrow.style.transform = 'rotate(0deg)';
-          }
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.arrow-button');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', function() {
+        const hiddenText = this.nextElementSibling;
+        if (hiddenText.style.display === 'none' || hiddenText.style.display === '') {
+          hiddenText.style.display = 'block';
+          this.querySelector('.arrow').innerHTML = '&#8593;'; // Mudando a seta para cima
+        } else {
+          hiddenText.style.display = 'none';
+          this.querySelector('.arrow').innerHTML = '&#8594;'; // Mudando a seta para direita
+        }
       });
+    });
   });
-});
+  
